e7f9b5ade60a37a225d691c079f74ea089ec5b71
unknown <sasankkolluuru@g,ail.com>
2026-03-25 20:14:06 +0530

    fix sqlite build

--- backend/database/dbService.js
+++ backend/database/dbService.js
@@ -1,5 +1,4 @@
-const Database = require("better-sqlite3");
-const db = new Database("database.db");
+const sqlite3 = require('sqlite3').verbose();
 const path = require('path');
 const fs = require('fs');
 
